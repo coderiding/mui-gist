ac828b1ddff73750e839b27141182abda25a504f
宇哥 <coderiding@gmail.com>
2024-11-22 11:41:37 +0800

    Update backdrop.tsx

--- feedback/backdrop/backdrop.tsx
+++ feedback/backdrop/backdrop.tsx
@@ -1,10 +1,10 @@
 // 导入头文件
 import { Backdrop, CircularProgress } from '@mui/material'
 
-// backdrop-true-false
+{/* backdrop-true-false */}
 const [loading, setLoading] = React.useState(false)
 
-// feedback-backdrop
+{/* feedback-backdrop */}
 <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
   <CircularProgress color="inherit" />
 </Backdrop>
